46fca700045f22bfa5ed81836fce2b8c39d784e6
Cole Pedersen <colep@lendio.com>
2019-04-29 16:36:49 -0600

    laravel collective form for contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,5 +2,21 @@
 
 @section('content')
     <h1>Contact</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eius fugit qui in aperiam laborum aspernatur culpa iusto provident laudantium.</p>
+    {{ Form::open(['url' => 'contact/submit']) }}
+        <div class="form-group">
+            {{Form::label('name', 'Name')}}
+            {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Enter name'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Enter email'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('message', 'Message')}}
+            {{Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Enter message'])}}
+        </div>
+        <div>
+            {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+        </div>
+    {{ Form::close() }}
 @endsection
